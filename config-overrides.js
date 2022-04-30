181515dfe4c37e19f833d09c39e1496388f5aff6
Kimiato <853447650@qq.com>
2022-05-01 00:45:28 +0800

    feat: init react ts proj

---
+++ config-overrides.js
@@ -0,0 +1,8 @@
+const { override } = require("customize-cra"); //override是覆写react隐藏的webpack配置
+const path = require("path");
+module.exports = override((config) => {
+  config.resolve.alias = {
+    "@": path.resolve(__dirname, "./src"),
+  };
+  return config;
+});
