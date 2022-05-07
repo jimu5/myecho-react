bd4df92ca3b436f4932337d3de89125a6a35f3ab
Kimiato <853447650@qq.com>
2022-05-08 00:41:31 +0800

    feat: 添加登录api

--- src/utils/apis/user.ts
+++ src/utils/apis/user.ts
@@ -1,4 +1,27 @@
+import axios from "axios";
+
 export interface user {
     id: number;
     nick_name: string;
+}
+
+export interface loginParams {
+    email: string;
+    name: string;
+    password: string;
+}
+
+export interface loginResponse {
+    email: string;
+    name: string;
+    nick_name: string;
+    last_login: string;
+    permission_type: number;
+    token: string;
+}
+
+export class User {
+    static login(params: loginParams) {
+        return axios.post("/login", params);
+    }
 }
