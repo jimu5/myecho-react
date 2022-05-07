2dbfc3234c5592babec03d9c883e9d3c15f62907
Kimiato <853447650@qq.com>
2022-05-08 00:41:22 +0800

    feat: 圆角属性adjust，登录页面路由

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -4,6 +4,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import s from './index.module.scss';
 
 const Home = lazy(() => import(/* webpackPrefetch:true */ '@/pages/Home'));
+const Login = lazy(() => import(/* webpackPrefetch:true */ '@/pages/Login'));
 
 const Main: React.FC = () => {
   return (
@@ -12,6 +13,7 @@ const Main: React.FC = () => {
         <Suspense fallback={<></>}>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </Suspense>
